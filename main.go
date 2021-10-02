79115452cdf784e051f76100d3bd008e11e4b399
kumar <kumarrockstarsaurabh@gmail.com>
2021-10-02 20:56:52 +0530

    Added ipv6 support

--- main.go
+++ main.go
@@ -12,6 +12,11 @@ import (
 	"sync"
 )
 
+type IPs struct {
+	Ports []string
+	IPv6  bool
+}
+
 func main() {
 	dir := flag.String("dir", "", "Path to directory to store xml outputs. Default: ~/scans/armada/")
 	flag.Parse()
@@ -28,14 +33,27 @@ func main() {
 		log.Println(err)
 	}
 
-	mm := make(map[string][]string)
+	mm := make(map[string]IPs)
 	sc := bufio.NewScanner(os.Stdin)
 	for sc.Scan() {
-		// Expecting only ipv4
-		// Expected input 127.0.0.1:443
+		// if there is more than one : then it's ipv6
 		ip := sc.Text()
 		tmp := strings.Split(ip, ":")
-		mm[tmp[0]] = append(mm[tmp[0]], tmp[1])
+		length := len(tmp)
+		if length == 2 {
+			tmpMap := mm[tmp[0]]
+			tmpMap.Ports = append(tmpMap.Ports, tmp[1])
+			tmpMap.IPv6 = false
+			mm[tmp[0]] = tmpMap
+		} else {
+			port := tmp[length-1]
+			ipv6 := strings.Join(tmp[:length-1], ":")
+			tmpMap := mm[ipv6]
+			tmpMap.Ports = append(tmpMap.Ports, port)
+			tmpMap.IPv6 = true
+			mm[ipv6] = tmpMap
+
+		}
 	}
 
 	threads := make(chan string, 30)
@@ -47,9 +65,15 @@ func main() {
 	}
 
 	for i, v := range mm {
-		ports := strings.Join(v, ",")
-		cmd := fmt.Sprintf("sudo nmap %s -p %s -oX ~/scans/armada/%s.xml --dns-servers `curl -s https://raw.githubusercontent.com/k-sau/resolvers/master/nmap-resolvers.txt` -Pn -sS --host-timeout 20m --open -T4 --max-retries 3 -sV", i, ports, i)
+		fmt.Println(i, v)
+		ports := strings.Join(v.Ports, ",")
+		cmd := ""
 
+		if v.IPv6 {
+			cmd = fmt.Sprintf("sudo nmap -6 %s -p %s -oX ~/scans/armada/%s.xml --dns-servers `curl -s https://raw.githubusercontent.com/k-sau/resolvers/master/nmap-resolvers.txt` -Pn -sS -w --host-timeout 20m --script-timeout 22m --open -T4 --max-retries 3 -sV", i, ports, i)
+		} else {
+			cmd = fmt.Sprintf("sudo nmap %s -p %s -oX ~/scans/armada/%s.xml --dns-servers `curl -s https://raw.githubusercontent.com/k-sau/resolvers/master/nmap-resolvers.txt` -Pn -sS --host-timeout 20m --script-timeout 22m --open -T4 --max-retries 3 -sV", i, ports, i)
+		}
 		wg.Add(1)
 		threads <- cmd
 	}
